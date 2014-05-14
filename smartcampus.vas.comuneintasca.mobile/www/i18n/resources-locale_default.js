8d46407d6c4993de859ccd42b68af2bd7f76069e
o.zambotti <o.zambotti@trentorise.eu>
2014-05-14 15:04:10 +0200

    New resources-locale strings

--- smartcampus.vas.comuneintasca.mobile/www/i18n/resources-locale_default.js
+++ smartcampus.vas.comuneintasca.mobile/www/i18n/resources-locale_default.js
@@ -1,47 +1,95 @@
 [
   {
-    "key":"Storia",
-    "value":"History",
-    "description":"side menu label"
+    "key": "sidemenu_Title",
+    "value": "Trento in Tasca",
+    "description": "side menu label"
   }, {
-    "key":"Eventi Principali",
-    "value":"Main Events",
-    "description":"side menu label"
+    "key": "sidemenu_Home",
+    "value": "Home",
+    "description": "side menu label"
   }, {
-    "key":"Mappa",
-    "value":"Map",
-    "description":"side menu label"
+    "key": "sidemenu_div_Conoscere",
+    "value": "Know Trento",
+    "description": "side menu label"
   }, {
-    "key":"Itinerari",
-    "value":"Itineraries",
-    "description":"side menu label"
+    "key": "sidemenu_div_Vivere",
+    "value": "Live Trento",
+    "description": "side menu label"
   }, {
-    "key":"Hotel",
-    "value":"Hotels",
-    "description":"side menu label"
+    "key": "sidemenu_div_Scoprire",
+    "value": "Discover Trento",
+    "description": "side menu label"
   }, {
-    "key":"Ristoranti",
-    "value":"Restaurants",
-    "description":"side menu label"
+    "key": "sidemenu_div_Percorsi",
+    "value": "Paths in Trento",
+    "description": "side menu label"
   }, {
-    "key":"Servizi",
-    "value":"Services",
-    "description":"side menu label"
+    "key": "sidemenu_div_Mangiare-dormire",
+    "value": "Eating and sleeping",
+    "description": "side menu label"
   }, {
-    "key":"restaurant_opening",
-    "value":"Opening",
-    "description":"restaurant leaf"
+    "key": "sidemenu_div_Info-utili",
+    "value": "Useful informations",
+    "description": "side menu label"
   }, {
-    "key":"restaurant_closing",
-    "value":"Closing days",
-    "description":"restaurant leaf"
+    "key": "sidemenu_Info",
+    "value": "Info",
+    "description": "side menu label"
   }, {
-    "key":"restaurant_price",
-    "value":"Price range",
-    "description":"restaurant leaf"
+    "key": "sidemenu_Storia",
+    "value": "History",
+    "description": "side menu label"
   }, {
-    "key":"restaurant_services",
-    "value":"Services",
-    "description":"restaurant leaf"
+    "key": "sidemenu_Concilio",
+    "value": "Concilio",
+    "description": "side menu label"
+  }, {
+    "key": "sidemenu_Eventi-Principali",
+    "value": "Main Events",
+    "description": "side menu label"
+  }, {
+    "key": "sidemenu_Bondone",
+    "value": "Mount Bondone",
+    "description": "side menu label"
+  }, {
+    "key": "sidemenu_Mappa",
+    "value": "Map",
+    "description": "side menu label"
+  }, {
+    "key": "sidemenu_Itinerari",
+    "value": "Itineraries",
+    "description": "side menu label"
+  }, {
+    "key": "sidemenu_Hotel",
+    "value": "Hotels",
+    "description": "side menu label"
+  }, {
+    "key": "sidemenu_Ristoranti",
+    "value": "Restaurants",
+    "description": "side menu label"
+  }, {
+    "key": "sidemenu_Servizi",
+    "value": "Services",
+    "description": "side menu label"
+  }, {
+    "key": "sidemenu_Uffici-comunali",
+    "value": "Municipality offices",
+    "description": "side menu label"
+  }, {
+    "key": "restaurant_opening",
+    "value": "Opening",
+    "description": "restaurant leaf"
+  }, {
+    "key": "restaurant_closing",
+    "value": "Closing days",
+    "description": "restaurant leaf"
+  }, {
+    "key": "restaurant_price",
+    "value": "Price range",
+    "description": "restaurant leaf"
+  }, {
+    "key": "restaurant_services",
+    "value": "Services",
+    "description": "restaurant leaf"
   }
 ]
--- smartcampus.vas.comuneintasca.mobile/www/i18n/resources-locale_it-IT.js
+++ smartcampus.vas.comuneintasca.mobile/www/i18n/resources-locale_it-IT.js
@@ -1,47 +1,87 @@
 [
   {
-    "key":"Storia",
-    "value":"Storia",
-    "description":"side menu label"
+    "key": "sidemenu_div_Conoscere",
+    "value": "Conoscere Trento",
+    "description": "side menu label"
   }, {
-    "key":"Eventi Principali",
-    "value":"Eventi Principali",
-    "description":"side menu label"
+    "key": "sidemenu_div_Vivere",
+    "value": "Vivere Trento",
+    "description": "side menu label"
   }, {
-    "key":"Mappa",
-    "value":"Mappa",
-    "description":"side menu label"
+    "key": "sidemenu_div_Scoprire",
+    "value": "Scoprire Trento",
+    "description": "side menu label"
   }, {
-    "key":"Itinerari",
-    "value":"Itinerari",
-    "description":"side menu label"
+    "key": "sidemenu_div_Percorsi",
+    "value": "Percorsi a Trento",
+    "description": "side menu label"
   }, {
-    "key":"Hotel",
-    "value":"Hotel",
-    "description":"side menu label"
+    "key": "sidemenu_div_Mangiare-dormire",
+    "value": "Mangiare e dormire",
+    "description": "side menu label"
   }, {
-    "key":"Ristoranti",
-    "value":"Ristoranti",
-    "description":"side menu label"
+    "key": "sidemenu_div_Info-utili",
+    "value": "Info utili",
+    "description": "side menu label"
   }, {
-    "key":"Servizi",
-    "value":"Servizi",
-    "description":"side menu label"
+    "key": "sidemenu_Info",
+    "value": "Info",
+    "description": "side menu label"
   }, {
-    "key":"restaurant_opening",
-    "value":"Orari",
-    "description":"restaurant leaf"
+    "key": "sidemenu_Storia",
+    "value": "Storia",
+    "description": "side menu label"
   }, {
-    "key":"restaurant_closing",
-    "value":"Giorni di chiusura",
-    "description":"restaurant leaf"
+    "key": "sidemenu_Concilio",
+    "value": "Concilio",
+    "description": "side menu label"
   }, {
-    "key":"restaurant_price",
-    "value":"Prezzo indicativo",
-    "description":"restaurant leaf"
+    "key": "sidemenu_Eventi-Principali",
+    "value": "Eventi Principali",
+    "description": "side menu label"
   }, {
-    "key":"restaurant_services",
-    "value":"Servizi disponibili",
-    "description":"restaurant leaf"
+    "key": "sidemenu_Bondone",
+    "value": "Monte Bondone",
+    "description": "side menu label"
+  }, {
+    "key": "sidemenu_Mappa",
+    "value": "Mappa",
+    "description": "side menu label"
+  }, {
+    "key": "sidemenu_Itinerari",
+    "value": "Itinerari",
+    "description": "side menu label"
+  }, {
+    "key": "sidemenu_Hotel",
+    "value": "Hotel",
+    "description": "side menu label"
+  }, {
+    "key": "sidemenu_Ristoranti",
+    "value": "Ristoranti",
+    "description": "side menu label"
+  }, {
+    "key": "sidemenu_Servizi",
+    "value": "Servizi",
+    "description": "side menu label"
+  }, {
+    "key": "sidemenu_Uffici-comunali",
+    "value": "Uffici Comunali",
+    "description": "side menu label"
+  }, {
+    "key": "restaurant_opening",
+    "value": "Orari",
+    "description": "restaurant leaf"
+  }, {
+    "key": "restaurant_closing",
+    "value": "Giorni di chiusura",
+    "description": "restaurant leaf"
+  }, {
+    "key": "restaurant_price",
+    "value": "Prezzo indicativo",
+    "description": "restaurant leaf"
+  }, {
+    "key": "restaurant_services",
+    "value": "Servizi disponibili",
+    "description": "restaurant leaf"
   }
 ]

545a8c766b00d618e13be739947580d31b372f13
raman <raman@fbk.eu>
2014-05-20 16:43:06 +0200

    data update

--- smartcampus.vas.comuneintasca.mobile/www/js/services.js
+++ smartcampus.vas.comuneintasca.mobile/www/js/services.js
@@ -220,7 +220,7 @@ angular.module('starter.services', [])
       return 'TrentoInTasca';
     },
     schemaVersion: function () {
-      return 59;
+      return 60;
     },
     syncTimeoutSeconds: function () {
       return 60 * 60; /* 60 times 60 seconds = 1 HOUR */

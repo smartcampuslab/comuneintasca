e4b7854e0ebd863e2f544d9f5e7bdaecf13878b2
o.zambotti <o.zambotti@trentorise.eu>
2014-05-20 15:10:21 +0200

    Fix for maps

--- smartcampus.vas.comuneintasca.mobile/www/js/services.js
+++ smartcampus.vas.comuneintasca.mobile/www/js/services.js
@@ -1203,21 +1203,28 @@ angular.module('starter.services', [])
 
 .factory('MapHelper', function ($location) {
   var map = {
+    control: {},
     draggable: 'true',
     center: {
-      latitude: 0,
-      longitude: 0
+      latitude: 45,
+      longitude: 11
     },
     zoom: 8,
     pan: false
   };
 
   var markers = {
-    models: [],
+    models: [{
+      latitude: 0,
+      longitude: 0
+    }, {
+      latitude: 0,
+      longitude: 0
+    }],
     coords: 'self',
     icon: 'icon',
     fit: true,
-    doCluster: false
+    doCluster: true
   };
 
   var showInfoWindow = false;
@@ -1253,6 +1260,7 @@ angular.module('starter.services', [])
   var categoriesIcons = {
     'dormire': 'home',
     'mangiare': 'restaurant',
+    'cultura': 'civic-building',
     'other': 'location'
   };
 

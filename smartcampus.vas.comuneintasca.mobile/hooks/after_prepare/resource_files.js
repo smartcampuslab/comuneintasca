6462b9fff9f68c30434220e9d7b64d7bb509bd27
Enrico Marchesin <github.com@palio.net>
2014-05-23 18:37:39 +0200

    ios splash screen localization

--- smartcampus.vas.comuneintasca.mobile/hooks/after_prepare/resource_files.js
+++ smartcampus.vas.comuneintasca.mobile/hooks/after_prepare/resource_files.js
@@ -14,7 +14,10 @@
 // and splash screen files here, even if 
 // we don't build for all platforms 
 // on each developer's box.
-var dirstocreate = ['platforms/android/res/values-en', 'platforms/android/res/values-de'];
+var dirstocreate = [
+	'platforms/android/res/values-en', 'platforms/android/res/values-de', 
+	'platforms/ios/Trento - Il Comune in Tasca/Resources/de.lproj', 'platforms/ios/Trento - Il Comune in Tasca/Resources/it.lproj'
+];
  
 var filestocopy = [{
     "config/android/AndroidManifest.xml": 
@@ -65,67 +68,91 @@ var filestocopy = [{
 
 {
     "config/ios/Resources/icons/icon-40@2x.png": 
-    "platforms/ios/Trento/Resources/icons/icon-40@2x.png"
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-40@2x.png"
 }, {
     "config/ios/Resources/icons/icon-40.png": 
-    "platforms/ios/Trento/Resources/icons/icon-40.png"
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-40.png"
 }, {
     "config/ios/Resources/icons/icon-50@2x.png": 
-    "platforms/ios/Trento/Resources/icons/icon-50@2x.png"
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-50@2x.png"
 }, {
     "config/ios/Resources/icons/icon-50.png": 
-    "platforms/ios/Trento/Resources/icons/icon-50.png"
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-50.png"
 }, {
     "config/ios/Resources/icons/icon-60@2x.png": 
-    "platforms/ios/Trento/Resources/icons/icon-60@2x.png"
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-60@2x.png"
 }, {
     "config/ios/Resources/icons/icon-60.png": 
-    "platforms/ios/Trento/Resources/icons/icon-60.png"
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-60.png"
 }, {
     "config/ios/Resources/icons/icon-72@2x.png": 
-    "platforms/ios/Trento/Resources/icons/icon-72@2x.png"
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-72@2x.png"
 }, {
     "config/ios/Resources/icons/icon-72.png": 
-    "platforms/ios/Trento/Resources/icons/icon-72.png"
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-72.png"
 }, {
     "config/ios/Resources/icons/icon-76@2x.png": 
-    "platforms/ios/Trento/Resources/icons/icon-76@2x.png"
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-76@2x.png"
 }, {
     "config/ios/Resources/icons/icon-76.png": 
-    "platforms/ios/Trento/Resources/icons/icon-76.png"
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-76.png"
 }, {
     "config/ios/Resources/icons/icon-small@2x.png": 
-    "platforms/ios/Trento/Resources/icons/icon-small@2x.png"
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-small@2x.png"
 }, {
     "config/ios/Resources/icons/icon-small.png": 
-    "platforms/ios/Trento/Resources/icons/icon-small.png"
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-small.png"
 }, {
     "config/ios/Resources/icons/icon@2x.png": 
-    "platforms/ios/Trento/Resources/icons/icon@2x.png"
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon@2x.png"
 }, {
     "config/ios/Resources/icons/icon.png": 
-    "platforms/ios/Trento/Resources/icons/icon.png"
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon.png"
 }, {
     "config/ios/Resources/splash/Default@2x~iphone.png": 
-    "platforms/ios/Trento/Resources/splash/Default@2x~iphone.png"
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default@2x~iphone.png"
+}, {
+    "config/ios/Resources/splash/Default_it@2x~iphone.png": 
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default_it@2x~iphone.png"
+}, {
+    "config/ios/Resources/splash/Default_de@2x~iphone.png": 
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default_de@2x~iphone.png"
 }, {
     "config/ios/Resources/splash/Default-568h@2x~iphone.png": 
-    "platforms/ios/Trento/Resources/splash/Default-568h@2x~iphone.png"
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default-568h@2x~iphone.png"
+}, {
+    "config/ios/Resources/splash/Default_it-568h@2x~iphone.png": 
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default_it-568h@2x~iphone.png"
+}, {
+    "config/ios/Resources/splash/Default_de-568h@2x~iphone.png": 
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default_de-568h@2x~iphone.png"
 }, {
     "config/ios/Resources/splash/Default~iphone.png":
-    "platforms/ios/Trento/Resources/splash/Default~iphone.png"
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default~iphone.png"
 }, {
-    "config/ios/Resources/splash/Default-Landscape@2x~ipad.png":
-    "platforms/ios/Trento/Resources/splash/Default-Landscape@2x~ipad.png"
+    "config/ios/Resources/splash/Default_it~iphone.png":
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default_it~iphone.png"
 }, {
-    "config/ios/Resources/splash/Default-Landscape~ipad.png":
-    "platforms/ios/Trento/Resources/splash/Default-Landscape~ipad.png"
+    "config/ios/Resources/splash/Default_de~iphone.png":
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default_de~iphone.png"
 }, {
-    "config/ios/Resources/splash/Default-Portrait@2x~ipad.png":
-    "platforms/ios/Trento/Resources/splash/Default-Portrait@2x~ipad.png"
+    "config/ios/Resources/it.lproj/InfoPlist.strings":
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/it.lproj/InfoPlist.strings"
 }, {
-    "config/ios/Resources/splash/Default-Portrait~ipad.png":
-    "platforms/ios/Trento/Resources/splash/Default-Portrait~ipad.png"
+    "config/ios/Resources/en.lproj/InfoPlist.strings":
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/en.lproj/InfoPlist.strings"
+}, {
+    "config/ios/Resources/de.lproj/InfoPlist.strings":
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/de.lproj/InfoPlist.strings"
+}, {
+    "config/ios/Resources/it.lproj/Localizable.strings":
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/it.lproj/Localizable.strings"
+}, {
+    "config/ios/Resources/en.lproj/Localizable.strings":
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/en.lproj/Localizable.strings"
+}, {
+    "config/ios/Resources/de.lproj/Localizable.strings":
+    "platforms/ios/Trento - Il Comune in Tasca/Resources/de.lproj/Localizable.strings"
 }, ];
 
 var fs = require('fs');

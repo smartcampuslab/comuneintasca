c84ee5883841aada93d039d69e479680c5ae876e
Enrico Marchesin <github.com@palio.net>
2014-06-19 13:43:00 +0200

    german translations fix

--- smartcampus.vas.comuneintasca.mobile/www/i18n/resources-locale_de.js
+++ smartcampus.vas.comuneintasca.mobile/www/i18n/resources-locale_de.js
@@ -72,7 +72,7 @@
     "description": "side menu label"
   }, {
     "key": "list_no-favourites",
-    "value": "Keine Favoriten gespeichert, aber",
+    "value": "Keine Lieblingsseiten gespeichert",
     "description": "favourites list results"
   }, {
     "key": "sidemenu_Info",

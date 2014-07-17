105756d24355a7e140faad5a4a1097576300d4ab
Enrico Marchesin <github.com@palio.net>
2014-07-17 17:10:54 +0200

    code cleanup

--- smartcampus.vas.comuneintasca.mobile/www/js/app.js
+++ smartcampus.vas.comuneintasca.mobile/www/js/app.js
@@ -348,6 +348,5 @@ angular.module('ilcomuneintasca', [
         }
       }
     });
-  $urlRouterProvider
-    .otherwise('/app/home'); // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/home'); // if none of the above states are matched, use this as the fallback
 });

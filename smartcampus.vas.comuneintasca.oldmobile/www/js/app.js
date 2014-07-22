3c7318ff6fd25267a50b080bec59b0b7cc4a1680
Enrico Marchesin <github.com@palio.net>
2014-07-22 18:26:36 +0200

    ios fixes

--- smartcampus.vas.comuneintasca.oldmobile/www/js/app.js
+++ smartcampus.vas.comuneintasca.oldmobile/www/js/app.js
@@ -3,44 +3,57 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'localization', 'ngCordova' ])
+angular.module('starter', ['ionic', 'localization', 'ngCordova'])
 
-.run(function($ionicPlatform, $rootScope, $cordovaDevice) {
-	$rootScope.appWebPage=function(){
-		window.open('http://www.comune.trento.it/Aree-tematiche/Turismo/Servizi-turistici/Il-Comune-in-tasca', '_system');
+.run(function ($ionicPlatform, $rootScope, $timeout) {
+	$ionicPlatform.ready(function () {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		}
+		if (window.StatusBar) {
+			StatusBar.styleDefault();
+		}
+		if (ionic.Platform.isWebView()) {
+			$timeout(function(){
+				document.addEventListener("deviceready", function () {
+					if (window.device) {
+						$rootScope.platform = window.device.platform;
+						//console.log('$rootScope.platform=' + $rootScope.platform);
+						$rootScope.iosPlatform = ($rootScope.platform == 'iOS');
+						$rootScope.androidPlatform = ($rootScope.platform == 'Android');
+					} else {
+						$rootScope.webPlatform = true;
+					}
+				});
+			});
+		} else {
+			//$rootScope.platform = 'iOS';
+			//$rootScope.iosPlatform = true;
+			$rootScope.webPlatform = true;
+		}
+	});
+
+	$rootScope.appDownload = function () {
+		if (ionic.Platform.isWebView()) {
+			if ($rootScope.platform == 'iOS') {
+				itmsurl='https://itunes.apple.com/app/id881529924';
+				console.log('opening  appstore: '+itmsurl);
+				window.open(itmsurl, '_system');
+			} else {
+				cordova.plugins.startapp.start({
+					android: 'it.smartcampuslab.comuni.trento'
+				}, function () {
+					console.log('START APP: success.');
+				}, function () {
+					console.log('START APP: failed!');
+					window.open('https://play.google.com/store/apps/details?id=it.smartcampuslab.comuni.trento', '_system');
+				});
+			}
+		} else {
+			window.open('http://www.comune.trento.it/Aree-tematiche/Turismo/Servizi-turistici/Il-Comune-in-tasca', '_system');
+		}
 	};
-	$rootScope.appDownload=function(){
-    if (ionic.Platform.isWebView()) {
-      cordova.plugins.startapp.start({
-        android: 'it.smartcampuslab.comuni.trento',
-				ios: 'https://itunes.apple.com/app/id881529924'
-      }, function () {
-        console.log('START APP: success.');
-      }, function () {
-        console.log('START APP: failed!');
 
-				if ($rootScope.platform == 'Android') {
-					window.open('https://play.google.com/store/apps/details?id=eu.trentorise.smartcampus.viaggiatrento', '_system');
-				} else if ($rootScope.platform == 'iOS') {
-					window.open('https://itunes.apple.com/app/id881529924', '_system');
-				} else {
-					$rootScope.appWebPage();
-				}
-      });
-    } else {
-			$rootScope.appWebPage();
-    }
-	};
-
-	$ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-		if (window.device) $rootScope.platform=device.platform;
-  });
 })

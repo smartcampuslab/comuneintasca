1a60aed2ca1f25c1b147426d16eb1a2aa9a1a643
Enrico Marchesin <github.com@palio.net>
2014-07-23 16:51:49 +0200

    minor updates

--- smartcampus.vas.comuneintasca.oldmobile/www/i18n/resources-locale_default.js
+++ smartcampus.vas.comuneintasca.oldmobile/www/i18n/resources-locale_default.js
@@ -4,9 +4,9 @@
   { "key": "appname2", "value": "The city in your pocket", "description": "" },
   { "key": "obsolete", "value": "obsolete", "description": "" },
   { "key": "txt1", "value": "This app version won't be updated anymore.", "description": "" },
-  { "key": "txt2_web", "value": "We kindly invite you to delete this version and download the new one:", "description": "" },
-  { "key": "txt2_ios", "value": "We kindly invite you to delete this version and download the new one from the Apple App Store:", "description": "" },
-  { "key": "txt2_android", "value": "We kindly invite you to delete this version and download the new one from Google Play:", "description": "" },
+  { "key": "txt2_web", "value": "We kindly invite you to uninstall this app and download the new one:", "description": "" },
+  { "key": "txt2_ios", "value": "We kindly invite you to uninstall this app and download the new one from the App Store:", "description": "" },
+  { "key": "txt2_android", "value": "We kindly invite you to uninstall this app and download the new one from Google Play:", "description": "" },
   { "key": "new_app", "value": "NEW APP!", "description": "" },
   { "key": "download", "value": "DOWNLOAD", "description": "" }
 ]

010e55ae6150e8f8fbe7fdecb78420209fd5bf12
Enrico Marchesin <github.com@palio.net>
2014-08-14 13:02:00 +0200

    automatic generation of icons and splashscreens (localized version to verify)

--- smartcampus.vas.comuneintasca.oldmobile/config/ticons/gulpfile.js
+++ smartcampus.vas.comuneintasca.oldmobile/config/ticons/gulpfile.js
@@ -1,37 +1,87 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var fs = require('fs');
 var sh = require('shelljs');
 var ticons = require('ticons');
 
-gulp.task('default', ['icons','splashes']);
+gulp.task('default', ['android-icons','android-splashes','iphone-icons','iphone-splashes']);
 
-gulp.task('icons', function(done) {
+gulp.task('android-icons', function(done) {
+  ticons.icons({
+    input: 'icon_rounded.png',
+    outputDir: '.',
+    platforms : ['android'],
+    classic: true, radius:10
+  }, function (err, output) {
+    if (err) {
+        throw err;
+    } else {
+      done();
+    }
+  });
+});
+
+gulp.task('android-splashes', function(done) {
+  var opts={
+    input: 'splash.png',
+    outputDir: '.',
+    platforms : ['android'],
+    classic: true, 
+    nine: false
+  };
+  ticons.splashes(opts, function (err, output) {
+    if (err) {
+        throw err;
+    } else {
+      if (fs.existsSync('./splash-en.png')) {
+        console.log('generating ENGLISH localized images')
+        opts.locale='en';
+        ticons.splashes(opts, function (err, output) {
+          if (err) {
+              throw err;
+          }
+        });
+      }
+      if (fs.existsSync('./splash-de.png')) {
+        console.log('generating DEUTSCHE localized images')
+        opts.locale='de';
+        ticons.splashes(opts, function (err, output) {
+          if (err) {
+              throw err;
+          }
+        });
+      }
+    }
+    done();
+  });
+});
+
+gulp.task('iphone-icons', function(done) {
   ticons.icons({
     input: 'icon.png',
     outputDir: '.',
-    targets: ['iphone','android'],
-    classic: true, 
+    platforms : ['iphone'],
+    classic: true
   }, function (err, output) {
     if (err) {
         throw err;
+    } else {
+      done();
     }
-    console.log('icons done');
   });
-  done();
 });
 
-gulp.task('splashes', function(done) {
+gulp.task('iphone-splashes', function(done) {
   ticons.splashes({
     input: 'splash.png',
     outputDir: '.',
-    targets: ['iphone','android'],
+    platforms : ['iphone'],
     classic: true, 
-    nine: false
   }, function (err, output) {
     if (err) {
         throw err;
+    } else {
+      done();
     }
-    console.log('splashes done');
   });
-  done();
 });

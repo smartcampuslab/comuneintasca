b0cf44df54549275f1c7f333f21c8f86c614cd47
gioadami <giordano.adami@gmail.com>
2014-09-04 15:53:00 +0200

    Exceptions.

---
+++ smartcampus.vas.comuneintasca.web/src/main/java/eu/trentorise/smartcampus/comuneintasca/data/BadDataException.java
@@ -0,0 +1,27 @@
+package eu.trentorise.smartcampus.comuneintasca.data;
+
+import java.util.List;
+import java.util.Map;
+
+public class BadDataException extends Exception {
+
+	public BadDataException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public BadDataException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BadDataException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BadDataException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}

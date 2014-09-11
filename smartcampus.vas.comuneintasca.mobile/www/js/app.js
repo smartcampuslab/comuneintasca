ce68001556147b0b4eb45f3315fbe4bc91f48c6b
Enrico Marchesin <github.com@palio.net>
2014-09-11 12:19:05 +0200

    code cleanup

--- smartcampus.vas.comuneintasca.mobile/www/js/app.js
+++ smartcampus.vas.comuneintasca.mobile/www/js/app.js
@@ -199,38 +199,31 @@ angular.module('ilcomuneintasca', [
         var b1 = $filter('translate')(b.title);
         var dif = a1.localeCompare(b1);
         return dif;
-      }
-      if ('Z-A' == order) {
+      } else if ('Z-A' == order) {
         var a1 = $filter('translate')(a.title);
         var b1 = $filter('translate')(b.title);
         var dif = b1.localeCompare(a1);
         return dif;
-      }
-      if ('Date' == order) {
+      } else if ('Date' == order) {
         var a1 = a.fromTime ? a.fromTime : a.fromDate;
         var b1 = b.fromTime ? b.fromTime : b.fromDate;
         var dif = b1 - a1;
         return dif;
-      }
-      if ('DateFrom' == order) {
+      } else if ('DateFrom' == order) {
         var a1 = a.fromTime ? a.fromTime : a.fromDate;
         var b1 = b.fromTime ? b.fromTime : b.fromDate;
         var dif = a1 - b1;
         return dif;
-      }
-      if ('DateTo' == order) {
+      } else if ('DateTo' == order) {
         var a1 = a.toTime ? a.toTime : a.toDate;
         var b1 = b.toTime ? b.toTime : b.toDate;
         var dif = a1 - b1;
         return dif;
-      }
-
-      if ('Distance' == order) {
+      } else if ('Distance' == order) {
         var a1 = a.distance;
         var b1 = b.distance;
         return a1 - b1;
-      }
-      if ('Stars' == order) {
+      } else if ('Stars' == order) {
         var a1 = a.stars || 0;
         var b1 = b.stars || 0;
         return b1 - a1;

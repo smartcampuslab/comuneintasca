56946a0a1a4456b761d59b15d21ca4f49e65b7c6
raman <raman@fbk.eu>
2014-09-11 17:01:28 +0200

    alignment

--- smartcampus.vas.comuneintasca.web/src/main/java/eu/trentorise/smartcampus/comuneintasca/processor/HomeProcessor.java
+++ smartcampus.vas.comuneintasca.web/src/main/java/eu/trentorise/smartcampus/comuneintasca/processor/HomeProcessor.java
@@ -2,6 +2,7 @@ package eu.trentorise.smartcampus.comuneintasca.processor;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import eu.trentorise.smartcampus.comuneintasca.model.ConfigObject;
 import eu.trentorise.smartcampus.comuneintasca.model.HomeObject;
 import eu.trentorise.smartcampus.network.JsonUtils;
 import eu.trentorise.smartcampus.presentation.common.exception.DataException;
@@ -26,19 +28,39 @@ public class HomeProcessor {
 	private Resource feedFile;
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
+	@Value("${configobject.file}")
+	private Resource configFile;
+
 	@Scheduled(fixedRate = 60000)
 	public void updateMessages() {
+		System.err.println(feedFile == null ? "NULL FEED": feedFile.getFilename());
+		System.err.println(configFile == null ? "NULL CONFIG": configFile.getFilename());
 
-		if (feedFile == null) return;
-
-		try {
-			String json = readJson();
-			HomeObject home = JsonUtils.toObject(json, HomeObject.class);
-			processObject(home);
-		} catch (Exception e) {
-			logger .error("Problem reading file: "+ e.getMessage());
+		if (feedFile != null) {
+			try {
+				String json = readJson();
+				HomeObject home = JsonUtils.toObject(json, HomeObject.class);
+				processObject(home);
+			} catch (Exception e) {
+				logger .error("Problem reading file: "+ e.getMessage());
+			}
+			
+			if (configFile == null) {
+				return;
+			}
 		}
 
+		if (configFile != null) {
+			try {
+				Long lastModified = configFile.lastModified();
+				String json = readJson(configFile.getInputStream());
+				ConfigObject home = JsonUtils.toObject(json, ConfigObject.class);
+				processConfig(home, lastModified);
+			} catch (Exception e) {
+				e.printStackTrace();
+				logger .error("Problem reading file: "+ e.getMessage());
+			}
+		}
 	}
 
 
@@ -71,4 +93,36 @@ public class HomeProcessor {
 		}
 		return json;
 	}
+	
+	private void processConfig(ConfigObject home, Long lastModified) throws DataException {
+		List<ConfigObject> oldList = storage.getObjectsByType(ConfigObject.class);
+		ConfigObject old = null;
+		home.setUpdateTime(lastModified);
+		if (oldList != null && !oldList.isEmpty()) {
+			old = oldList.get(0);
+			if (old.getUpdateTime() < lastModified) {
+				home.setId(old.getId());
+				storage.storeObject(home);
+			}
+		} else {
+			storage.storeObject(home);
+		}
+	}
+
+
+	private String readJson(InputStream is) throws IOException {
+		String json = "";
+		BufferedReader bin = null;
+		try {
+			bin = new BufferedReader(new InputStreamReader(is));
+			String line = null;
+			while ((line = bin.readLine()) != null) {
+				json += line + "\n";
+			}
+		} finally {
+			if (bin != null) bin.close();
+		}
+		return json;
+	}
+
 }

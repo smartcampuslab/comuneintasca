6146c8dcdbff5cb2ac4a149da373f4967b576bc8
Enrico Marchesin <github.com@palio.net>
2014-09-24 17:19:06 +0200

    obsolete app text fixes

--- smartcampus.vas.comuneintasca.oldmobile/www/i18n/resources-locale_it.js
+++ smartcampus.vas.comuneintasca.oldmobile/www/i18n/resources-locale_it.js
@@ -2,11 +2,12 @@
   { "key": "_suffix", "value": "_it", "description": "" },
   { "key": "appname1", "value": "Trento", "description": "" },
   { "key": "appname2", "value": "Il Comune in tasca", "description": "" },
-  { "key": "obsolete", "value": "obsoleta", "description": "" },
+  { "key": "obsolete", "value": "OBSOLETA", "description": "" },
   { "key": "txt1", "value": "Questa versione dell'app non verrà più aggiornata.", "description": "" },
-  { "key": "txt2_web", "value": "È consigliabile disinstallare questa versione dell'app e scaricare quella nuova:", "description": "" },
-  { "key": "txt2_ios", "value": "È consigliabile disinstallare questa versione dell'app e scaricare quella nuova da App Store:", "description": "" },
-  { "key": "txt2_android", "value": "È consigliabile cancellare questa versione dell'app e scaricare quella nuova da Google Play:", "description": "" },
+  { "key": "txt2_web", "value": "È consigliabile disinstallarla,", "description": "" },
+  { "key": "txt2_ios", "value": "È consigliabile disinstallarla,", "description": "" },
+  { "key": "txt2_android", "value": "Puoi disinstallarla <span class=\"color\">QUI</span>,", "description": "" },
+  { "key": "txt3", "value": "e scaricare la nuova versione qui sotto:", "description": "" },
   { "key": "new_app", "value": "NUOVA APP!", "description": "" },
   { "key": "download", "value": "SCARICA", "description": "" }
 ]

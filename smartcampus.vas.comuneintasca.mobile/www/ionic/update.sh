729fd07ed3df6c293826f57648c6e8b053c742af
Enrico Marchesin <github.com@palio.net>
2014-10-07 16:31:28 +0200

    updated to latest ionic beta

--- smartcampus.vas.comuneintasca.mobile/www/ionic/update.sh
+++ smartcampus.vas.comuneintasca.mobile/www/ionic/update.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #VERSION="nightly"
 #VERSION="1.0.0-beta.6"
-VERSION="1.0.0-beta.11"
+#VERSION="1.0.0-beta.11"
+VERSION="1.0.0-beta.13"
 curl "http://code.ionicframework.com/$VERSION/css/ionic.min.css" >ionic.min.css
 curl "http://code.ionicframework.com/$VERSION/js/ionic.bundle.min.js" >ionic.bundle.min.js

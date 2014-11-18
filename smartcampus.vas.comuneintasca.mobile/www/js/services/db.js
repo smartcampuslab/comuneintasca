5250c9c6897c71535a79261a1be7c221080ad2e0
Enrico Marchesin <github.com@palio.net>
2014-11-18 13:47:09 +0100

    fixed filter cleanup implementation

--- smartcampus.vas.comuneintasca.mobile/www/js/services/db.js
+++ smartcampus.vas.comuneintasca.mobile/www/js/services/db.js
@@ -1208,22 +1208,24 @@ angular.module('ilcomuneintasca.services.db', [])
       //console.log('types[type]: '+types[type]);
 
       db.then(function (dbObj) {
-        Profiling._do('filterclean','dbobj');
+        //Profiling._do('filterclean','dbobj');
         
         dbObj.transaction(function (tx) {
           Profiling._do('filterclean','dbtrans');
           
           var cleaned={};
+          var cleanedPromises=[];
           for (key in cates) {
-            cleaned[key]=$q.defer();
+            var d=$q.defer();
+            cleaned[key]=d;
+            cleanedPromises.push(d.promise);
           }
-          Profiling._do('filterclean','promises');
+          //Profiling._do('filterclean','promises');
 
-          var filteredCates=[];
           for (var key in cates) {
             var cate=cates[key].it;
             //console.log('cates['+key+']: '+cate);
-            var dbQuery="select distinct '"+key+"' as catekey from ContentObjects where type=? and (classification=? OR classification2=? OR classification3=?)";
+            var dbQuery="select count(*) as totale, '"+key+"' as catekey from ContentObjects where type=? and (classification=? OR classification2=? OR classification3=?)";
             //console.log('dbQuery: '+dbQuery);
             var dbQueryArgs=[ types[type], key, key, key ];
             //console.log('dbQueryArgs: '+dbQueryArgs);
@@ -1232,34 +1234,57 @@ angular.module('ilcomuneintasca.services.db', [])
               //console.log('resultslen='+resultslen);
               if (resultslen>0) {
                 var item = results.rows.item(0);
-                //console.log('cates['+item.catekey+']='+[item.catekey]);
+                //console.log('cates['+item.catekey+']='+item.catekey);
+
                 //filteredCates.push(cates[item.catekey]);
-                filteredCates[item.catekey]=cates[item.catekey];
-                Profiling._do('filterclean', 'done');
-                cleaned[item.catekey].resolve();
+                //filteredCates[item.catekey]=cates[item.catekey];
+                //cleaned[item.catekey].resolve();
+
+                //console.log('cates['+item.catekey+'].count='+item.totale);
+                Profiling._do('filterclean', 'done.'+item.catekey);
+                if (item.totale>0) {
+                  cleaned[item.catekey].resolve(item.catekey);
+                } else {
+                  cleaned[item.catekey].resolve(null);
+                }
               } else {
                 //console.log('no results for cates['+key+']: ');
-                Profiling._do('filterclean', 'err1');
+                Profiling._do('filterclean', 'err.'+key);
                 cleaned[key].reject();
               }
             }, function (tx, err) {
               console.log('error: ' + err);
-              Profiling._do('filterclean', 'sqlerror');
+              Profiling._do('filterclean', 'sql tx2 error');
               cleaned[key].reject();
             });
           }
 
-          $q.all(cleaned).then(function() {
+          $q.all(cleanedPromises).then(function(cleanedPromisesCates) {
+            //console.log('cleanedPromisesCates: '+cleanedPromisesCates);
+            Profiling._do('filterclean', 'q.all');
+
+            var filteredCates={};
+            for (i in cleanedPromisesCates) {
+              var key=cleanedPromisesCates[i];
+              if (key!=null) filteredCates[key]=cates[key];
+            }
+            //console.log('filteredCates keys: '+Object.keys(filteredCates));
+
             returned.resolve(filteredCates);
+          },function(err){
+            console.log('$q.all() error: ' + err);
+            Profiling._do('filterclean', 'q.all error');
+            returned.reject(err);
           });
         }, function (error) { //error callback
           Profiling._do('filterclean', 'tx error');
           console.log('db.cleanupCatesOfType() ERROR: ' + error);
           returned.reject(error);
         }, function () { //success callback
-          Profiling._do('filterclean', 'tx success');
+          Profiling._do('filterclean', 'sql tx1 success');
         });
       },function (error) {
+        console.log('dbobj ERROR: ' + error);
         returned.reject(error);
       });
 

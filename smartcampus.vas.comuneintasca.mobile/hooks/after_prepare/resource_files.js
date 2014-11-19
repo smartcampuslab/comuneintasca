86dbd11b34c24282e8c93a947357e59a9ece3977
Enrico Marchesin <github.com@palio.net>
2014-11-19 12:37:16 +0100

    complete refactoring of cordova hook for resource copying

--- smartcampus.vas.comuneintasca.mobile/hooks/after_prepare/resource_files.js
+++ smartcampus.vas.comuneintasca.mobile/hooks/after_prepare/resource_files.js
@@ -1,31 +1,21 @@
 #!/usr/bin/env node
+var fs = require('fs');
+var path = require('path');
  
-//
-// This hook copies various resource files 
-// from our version control system directories 
-// into the appropriate platform specific location
-//
- 
- 
-// configure all the files to copy.  
-// Key of object is the source file, 
-// value is the destination location.  
-// It's fine to put all platforms' icons 
-// and splash screen files here, even if 
-// we don't build for all platforms 
-// on each developer's box.
-var dirstocreate = [
+
+var NOME_COMUNE='Trento';
+var defaultLang='en';
+var rootdir = process.argv[2];
+
+
+var androidDirsToCreate = [
 	'platforms/android/res/values', 'platforms/android/res/values-it', 'platforms/android/res/values-de', 
-	'platforms/android/res/drawable-mdpi', 'platforms/android/res/drawable-hdpi', 'platforms/android/res/drawable-xhdpi', 'platforms/android/res/drawable-xxhdpi', 
-	'platforms/ios/Trento - Il Comune in Tasca/Resources/de.lproj', 'platforms/ios/Trento - Il Comune in Tasca/Resources/it.lproj'
-];
- 
-var filestocopy = [{
+	'platforms/android/res/drawable-mdpi', 'platforms/android/res/drawable-hdpi', 'platforms/android/res/drawable-xhdpi', 'platforms/android/res/drawable-xxhdpi'
+]; 
+var androidFilesToCopy = [{
     "config/android/AndroidManifest.xml": 
     "platforms/android/AndroidManifest.xml"
-}, 
-
-{
+}, {
     "config/android/res/values-en/strings.xml": 
     "platforms/android/res/values/strings.xml"
 }, {
@@ -34,9 +24,7 @@ var filestocopy = [{
 }, {
     "config/android/res/values-de/strings.xml": 
     "platforms/android/res/values-de/strings.xml"
-},
-
-{
+}, {
     "config/android/res/drawable-mdpi/ic_action_next_item.png": 
     "platforms/android/res/drawable-mdpi/ic_action_next_item.png"
 }, {
@@ -72,222 +60,199 @@ var filestocopy = [{
 }, {
     "config/android/res/drawable-xxhdpi/ic_action_remove.png": 
     "platforms/android/res/drawable-xxhdpi/ic_action_remove.png"
-}, 
+}];
 
-{
-    "config/ios/Resources/icons/icon-40@2x.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-40@2x.png"
+var iosDirsToCreate = [
+	'platforms/ios/'+NOME_COMUNE+' - Il Comune in Tasca/Resources/de.lproj', 'platforms/ios/'+NOME_COMUNE+' - Il Comune in Tasca/Resources/it.lproj'
+];
+var iosFilesToCopy = [{
+    "config/ticons/Resources/iphone/appicon-Small-40@2x.png": 
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/icons/icon-40@2x.png"
 }, {
-    "config/ios/Resources/icons/icon-40.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-40.png"
+    "config/ticons/Resources/iphone/appicon-Small-40.png": 
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/icons/icon-40.png"
 }, {
-    "config/ios/Resources/icons/icon-50@2x.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-50@2x.png"
+    "config/ticons/Resources/iphone/appicon-Small-50@2x.png": 
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/icons/icon-50@2x.png"
 }, {
-    "config/ios/Resources/icons/icon-50.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-50.png"
+    "config/ticons/Resources/iphone/appicon-Small-50.png": 
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/icons/icon-50.png"
 }, {
-    "config/ios/Resources/icons/icon-60@2x.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-60@2x.png"
+    "config/ticons/Resources/iphone/appicon-60@3x.png": 
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/icons/icon-60@3x.png"
 }, {
-    "config/ios/Resources/icons/icon-60.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-60.png"
+    "config/ticons/Resources/iphone/appicon-60@2x.png": 
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/icons/icon-60@2x.png"
 }, {
-    "config/ios/Resources/icons/icon-72@2x.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-72@2x.png"
+    "config/ticons/Resources/iphone/appicon-60.png": 
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/icons/icon-60.png"
 }, {
-    "config/ios/Resources/icons/icon-72.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-72.png"
+    "config/ticons/Resources/iphone/appicon-72@2x.png": 
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/icons/icon-72@2x.png"
 }, {
-    "config/ios/Resources/icons/icon-76@2x.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-76@2x.png"
+    "config/ticons/Resources/iphone/appicon-72.png": 
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/icons/icon-72.png"
 }, {
-    "config/ios/Resources/icons/icon-76.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-76.png"
+    "config/ticons/Resources/iphone/appicon-76@2x.png": 
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/icons/icon-76@2x.png"
 }, {
-    "config/ios/Resources/icons/icon-small@2x.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-small@2x.png"
+    "config/ticons/Resources/iphone/appicon-76.png": 
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/icons/icon-76.png"
 }, {
-    "config/ios/Resources/icons/icon-small.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon-small.png"
+    "config/ticons/Resources/iphone/appicon-Small@3x.png": 
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/icons/icon-small@3x.png"
 }, {
-    "config/ios/Resources/icons/icon@2x.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon@2x.png"
+    "config/ticons/Resources/iphone/appicon-Small@2x.png": 
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/icons/icon-small@2x.png"
 }, {
-    "config/ios/Resources/icons/icon.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/icons/icon.png"
+    "config/ticons/Resources/iphone/appicon-Small.png": 
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/icons/icon-small.png"
 }, {
-    "config/ios/Resources/splash/Default@2x~iphone.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default@2x~iphone.png"
+    "config/ticons/Resources/iphone/appicon@2x.png": 
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/icons/icon@2x.png"
 }, {
-    "config/ios/Resources/splash/Default_en@2x~iphone.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default_en@2x~iphone.png"
-}, {
-    "config/ios/Resources/splash/Default_it@2x~iphone.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default_it@2x~iphone.png"
-}, {
-    "config/ios/Resources/splash/Default_de@2x~iphone.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default_de@2x~iphone.png"
-}, {
-    "config/ios/Resources/splash/Default-568h@2x~iphone.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default-568h@2x~iphone.png"
-}, {
-    "config/ios/Resources/splash/Default_en-568h@2x~iphone.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default_en-568h@2x~iphone.png"
-}, {
-    "config/ios/Resources/splash/Default_it-568h@2x~iphone.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default_it-568h@2x~iphone.png"
-}, {
-    "config/ios/Resources/splash/Default_de-568h@2x~iphone.png": 
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default_de-568h@2x~iphone.png"
-}, {
-    "config/ios/Resources/splash/Default~iphone.png":
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default~iphone.png"
-}, {
-    "config/ios/Resources/splash/Default_en~iphone.png":
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default_en~iphone.png"
-}, {
-    "config/ios/Resources/splash/Default_it~iphone.png":
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default_it~iphone.png"
-}, {
-    "config/ios/Resources/splash/Default_de~iphone.png":
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/splash/Default_de~iphone.png"
+    "config/ticons/Resources/iphone/appicon.png": 
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/icons/icon.png"
 }, {
     "config/ios/Resources/it.lproj/InfoPlist.strings":
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/it.lproj/InfoPlist.strings"
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/it.lproj/InfoPlist.strings"
 }, {
     "config/ios/Resources/en.lproj/InfoPlist.strings":
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/en.lproj/InfoPlist.strings"
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/en.lproj/InfoPlist.strings"
 }, {
     "config/ios/Resources/de.lproj/InfoPlist.strings":
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/de.lproj/InfoPlist.strings"
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/de.lproj/InfoPlist.strings"
 }, {
     "config/ios/Resources/it.lproj/Localizable.strings":
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/it.lproj/Localizable.strings"
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/it.lproj/Localizable.strings"
 }, {
     "config/ios/Resources/en.lproj/Localizable.strings":
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/en.lproj/Localizable.strings"
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/en.lproj/Localizable.strings"
 }, {
     "config/ios/Resources/de.lproj/Localizable.strings":
-    "platforms/ios/Trento - Il Comune in Tasca/Resources/de.lproj/Localizable.strings"
-}, ];
+    "platforms/ios/"+NOME_COMUNE+" - Il Comune in Tasca/Resources/de.lproj/Localizable.strings"
+} ];
 
-var fs = require('fs');
-var path = require('path');
- 
-// no need to configure below
-var rootdir = process.argv[2];
 
-dirstocreate.forEach(function(d) {
-  var destdir = path.join(rootdir, d);
-  if (!fs.existsSync(destdir)) {
-    fs.mkdirSync(destdir);
-  }
-});
- 
-filestocopy.forEach(function(obj) {
-    Object.keys(obj).forEach(function(key) {
-        var val = obj[key];
-        var srcfile = path.join(rootdir, key);
-        var destfile = path.join(rootdir, val);
+var copySrcToDestOnlyIfNeeded=function(srcfile,destfile,createdestdir) {
+  if (fs.existsSync(srcfile)) {
+    var destdir = path.dirname(destfile);
+    if (createdestdir && !fs.existsSync(destdir)) {
+      console.log('creating dest dir: '+destdir);
+      fs.mkdirSync(destdir);
+    }
+    if (fs.existsSync(destdir)) {
+      var srcstats=fs.statSync(srcfile);
+      var deststats=null;
+      if (fs.existsSync(destfile)) deststats=fs.statSync(destfile);
+      if (deststats==null || srcstats.mtime.getTime()!=deststats.mtime.getTime()) {
         console.log("copying "+srcfile+" to "+destfile);
-        var destdir = path.dirname(destfile);
-        if (fs.existsSync(srcfile) && fs.existsSync(destdir)) {
-        	try {
-	          fs.createReadStream(srcfile).pipe( fs.createWriteStream(destfile) );
-					} catch(e) {
-						console.log('ERROR0: '+e);
-					}
+        try {
+          var reader=fs.createReadStream(srcfile);
+          reader.once('end', function () {
+            fs.utimesSync(destfile, srcstats.atime, srcstats.mtime);
+          });
+          reader.pipe(fs.createWriteStream(destfile));
+        } catch(e) {
+          console.log('*** COPY ERROR #0: '+e);
         }
-    });
-});
+      } else {
+        //console.log("...skip copying "+srcfile+" to "+destfile);
+      }
+    } else {
+      console.log('*** missing destination dir: '+destdir);
+    }
+  } else {
+    console.log('*** missing source file: '+srcfile);
+  }
+}
 
-/*
-['','en-','de-']
-.forEach(function(lang) {
-    ['long-port-hdpi', 'long-port-ldpi', 'long-port-xhdpi', 'notlong-port-hdpi', 'notlong-port-ldpi', 'notlong-port-xhdpi', 'notlong-port-mdpi']
-    .forEach(function(d) {
-      var destdir = path.join(rootdir, 'platforms/android/res/drawable-'+lang + d);
-      var srcdir = path.join(rootdir, 'config/android/res/drawable-'+lang + d);
-      if (!fs.existsSync(destdir)) {
-        fs.mkdirSync(destdir);
-        var srcfile = path.join(srcdir,'splash.png');
-        var destfile = path.join(destdir,'splash.png');
-         if (fs.existsSync(srcfile)) {
-            fs.createReadStream(srcfile).pipe(
-               fs.createWriteStream(destfile));
-        }
+
+if (fs.existsSync(path.join(rootdir, 'platforms/ios'))) {
+  iosDirsToCreate.forEach(function(d) {
+    var destdir = path.join(rootdir, d);
+    if (!fs.existsSync(destdir)) fs.mkdirSync(destdir);
+  });
+  iosFilesToCopy.forEach(function(obj) {
+    Object.keys(obj).forEach(function(key) {
+      var val = obj[key];
+      var srcfile = path.join(rootdir, key);
+      var destfile = path.join(rootdir, val);
+      copySrcToDestOnlyIfNeeded(srcfile,destfile,false);
+    });
+  });
+
+  ['','@2x','-568h@2x'] //6&6+ resulutions ,'-667h@2x','-Landscape-736h@3x','-Portrait-736h@3x'
+  .forEach(function(suffix) {
+    var def_srcfile = path.join(rootdir, 'config/ticons/Resources/iphone', 'Default'+ suffix +'.png');
+    copySrcToDestOnlyIfNeeded(def_srcfile,path.join(rootdir, 'platforms/ios/'+NOME_COMUNE+' - Il Comune in Tasca/Resources/splash', 'Default'+ suffix +'~iphone.png'),false);
+
+    ['en','it','de']
+    .forEach(function(lang) {
+      var srcfile = path.join(rootdir, 'config/ticons/i18n', lang, 'Default'+ suffix +'.png');
+      copySrcToDestOnlyIfNeeded(srcfile,path.join(rootdir, 'platforms/ios/'+NOME_COMUNE+' - Il Comune in Tasca/Resources/splash', 'Default_'+lang + suffix +'~iphone.png'),false);
+    }); 
+  });
+
+  ['-Landscape','-Landscape@2x','-Portrait','-Portrait@2x']
+  .forEach(function(suffix) {
+    var def_srcfile = path.join(rootdir, 'config/ticons/Resources/iphone', 'Default'+ suffix +'.png');
+    copySrcToDestOnlyIfNeeded(def_srcfile,path.join(rootdir, 'platforms/ios/'+NOME_COMUNE+' - Il Comune in Tasca/Resources/splash', 'Default'+ suffix +'~ipad.png'),false);
+
+    ['en','it','de']
+    .forEach(function(lang) {
+      var srcfile = path.join(rootdir, 'config/ticons/i18n', lang, 'Default'+ suffix +'.png');
+      copySrcToDestOnlyIfNeeded(srcfile,path.join(rootdir, 'platforms/ios/'+NOME_COMUNE+' - Il Comune in Tasca/Resources/splash', 'Default_'+lang + suffix +'~ipad.png'),false);
+    }); 
+  });
+} else {
+  console.log('skipping IOS resources');
+}
+
+
+if (fs.existsSync(path.join(rootdir, 'platforms/android'))) {
+  androidDirsToCreate.forEach(function(d) {
+    var destdir = path.join(rootdir, d);
+    if (!fs.existsSync(destdir)) fs.mkdirSync(destdir);
+  });
+  androidFilesToCopy.forEach(function(obj) {
+    Object.keys(obj).forEach(function(key) {
+      var val = obj[key];
+      var srcfile = path.join(rootdir, key);
+      var destfile = path.join(rootdir, val);
+      copySrcToDestOnlyIfNeeded(srcfile,destfile,false);
+    });
+  });
+
+  ['l','m','h','xh','xxh']//,'xxxh'
+  .forEach(function(d) {
+    var suffix='-' + d + 'dpi';
+    var srcfile = path.join(rootdir, 'config/ticons/platform/android/res/drawable' + suffix, 'appicon.png');
+    var destfile = path.join(rootdir, 'platforms/android/res/drawable' + suffix, 'icon.png');
+    copySrcToDestOnlyIfNeeded(srcfile,destfile,true);
+
+    ['en','it','de']
+    .forEach(function(lang) {
+      var suffix='-' + lang + '-' + d + 'dpi';
+      var srcfile = path.join(rootdir, 'config/ticons/platform/android/res/drawable' + suffix, 'background.9.png');
+      if (fs.existsSync(srcfile)) {
+        if (lang==defaultLang) suffix='-' + d + 'dpi';
+        copySrcToDestOnlyIfNeeded(srcfile,path.join(rootdir, 'platforms/android/res/drawable' + suffix, 'splash.png'),true);
+      } else {
+        ['','not']
+        .forEach(function(ll) {
+          ['land','port']
+          .forEach(function(lp) {
+            suffix='-'+ll+'long' + '-'+lp + '-'+d+'dpi';
+            srcfile = path.join(rootdir, 'config/ticons/Resources/android/images/res-'+lang+suffix, 'default.png');
+
+            if (lang!=defaultLang) suffix='-'+lang+suffix;
+            copySrcToDestOnlyIfNeeded(srcfile,path.join(rootdir, 'platforms/android/res/drawable' + suffix, 'splash.png'),true);
+          });
+        });
       }
     }); 
-});
-*/
-
-var defaultLang='en';
-
-['l','m','h','xh','xxh']//,'xxxh'
-.forEach(function(d) {
-	var suffix='-' + d + 'dpi';
-	var srcdir = path.join(rootdir, 'config/ticons/platform/android/res/drawable' + suffix);
-	var destdir = path.join(rootdir, 'platforms/android/res/drawable' + suffix);
-	if (!fs.existsSync(destdir)) {
-		console.log('creating dir: '+destdir);
-		fs.mkdirSync(destdir);
-	}
-	var srcfile = path.join(srcdir,'appicon.png');
-	if (fs.existsSync(srcfile)) {
-		console.log('copying file: '+srcfile);
-		var destfile = path.join(destdir,'icon.png');
-		console.log('to: '+destfile);
-		try {
-			fs.createReadStream(srcfile).pipe( fs.createWriteStream(destfile) );
-		} catch(e) {
-			console.log('ERROR1: '+e);
-		}
-	}
-
-	['en','it','de']
-	.forEach(function(lang) {
-		var suffix='-' + lang + '-' + d + 'dpi';
-		var srcdir = path.join(rootdir, 'config/ticons/platform/android/res/drawable' + suffix);
-		var srcfile = path.join(srcdir,'background.9.png');
-		if (fs.existsSync(srcfile)) {
-			if (lang==defaultLang) suffix='-' + d + 'dpi';
-			var destdir = path.join(rootdir, 'platforms/android/res/drawable' + suffix);
-			if (!fs.existsSync(destdir)) {
-				console.log('creating dir: '+destdir);
-				fs.mkdirSync(destdir);
-			}
-			console.log('copying file: '+srcfile);
-			var destfile = path.join(destdir,'splash.png');
-			console.log('to: '+destfile);
-			fs.createReadStream(srcfile).pipe( fs.createWriteStream(destfile) );
-
-		} else {
-			['','not']
-			.forEach(function(ll) {
-				['land','port']
-				.forEach(function(lp) {
-					suffix='-'+ll+'long' + '-'+lp + '-'+d+'dpi';
-					srcdir = path.join(rootdir, 'config/ticons/Resources/android/images/res-'+lang+suffix);
-					srcfile = path.join(srcdir,'default.png');
-					if (fs.existsSync(srcfile)) {
-						if (lang!=defaultLang) suffix='-'+lang+suffix;
-						var destdir = path.join(rootdir, 'platforms/android/res/drawable' + suffix);
-						if (!fs.existsSync(destdir)) {
-							console.log('creating dir: '+destdir);
-							fs.mkdirSync(destdir);
-						}
-						console.log('copying file: '+srcfile);
-						var destfile = path.join(destdir,'splash.png');
-						console.log('to: '+destfile);
-						try {
-							fs.createReadStream(srcfile).pipe( fs.createWriteStream(destfile) );
-						} catch(e) {
-							console.log('ERROR2: '+e);
-						}
-					}
-				});
-			});
-		}
-	}); 
-});
+  });
+} else {
+  console.log('skipping ANDROID resources');
+}

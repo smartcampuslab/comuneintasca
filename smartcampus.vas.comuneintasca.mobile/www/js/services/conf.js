0858209e17a962f8db384aa6d184ece6800a06a9
Enrico Marchesin <github.com@palio.net>
2014-12-02 15:41:08 +0100

    build version bump

--- smartcampus.vas.comuneintasca.mobile/www/js/services/conf.js
+++ smartcampus.vas.comuneintasca.mobile/www/js/services/conf.js
@@ -10,7 +10,7 @@ angular.module('ilcomuneintasca.services.conf', [])
   // $rootScope.DEV=true;
 
   var APP_VERSION='3.1.0';
-  var APP_BUILD='141126.1626';
+  var APP_BUILD='141202.1448';
   
   var SYNC_WEBAPP='comuneintasca-multi';
   var SCHEMA_VERSION=110;
